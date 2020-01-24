186b06a357f7d35bdf24a6189d511e3dc59c9b76
Aldren Terante <aldren.terante@gmail.com>
2020-01-24 22:36:46 +0800

    feat: integrate antd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import Title from '@components/Title';
+import { DatePicker } from 'antd';
 
-export default () => <Title>My page</Title>;
+export default () => (
+  <React.Fragment>
+    <Title>My page</Title>
+    <DatePicker />
+  </React.Fragment>
+);
